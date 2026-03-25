c63e48b0b5c65f627d7d8e69a865a7fbb2c7a4ca
Sharon Moegi <sharamoegi@gmail.com>
2026-03-25 16:16:43 +0300

    Test convertToUpperCAse function

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-let amount;
 function calculateTax(amount) {
     const taxRate = 0.1; // 10% tax
     return amount * taxRate;
 }
 //Test calculateTax
 let tax = calculateTax(100);
-console.log(`Tax on ${amount}: ${tax}`);
+console.log(`Tax on 100: ${tax}`);
 
-// Function to convert a string to uppercase
+// Function to convert text to uppercase
 function convertToUpperCase(text) {
     return text.toUpperCase();
 }
+//Test convertToUpperCase
+let name = convertToUpperCase(`name`);
+console.log(`Uppercase of 'name': ${name}`);
 
 
 
