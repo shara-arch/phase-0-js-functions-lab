39704604c53c32455426e2dfeedd9939865205e1
Sharon Moegi <sharamoegi@gmail.com>
2026-03-25 16:01:41 +0300

    Test calculateTax function

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ function calculateTax(amount) {
     const taxRate = 0.1; // 10% tax
     return amount * taxRate;
 }
+//Test calculateTax
+let tax = calculateTax(100);
+console.log(`Tax on 100: ${tax}`);
 
 
 
